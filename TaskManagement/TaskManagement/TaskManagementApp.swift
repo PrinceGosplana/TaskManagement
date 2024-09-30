d2806e9ed4ce8f670edede18221346dd62d49087
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-09-30 10:28:04 +0300

    init

---
+++ TaskManagement/TaskManagement/TaskManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagementApp.swift
+//  TaskManagement
+//
+//  Created by Oleksandr Isaiev on 30.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
